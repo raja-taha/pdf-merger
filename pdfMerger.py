6db4b6352f193d5239ee7a34da7d6d295d01e89a
Mr_RajaG <rajataha062@gmail.com>
2024-01-13 16:36:12 +0500

    PDF Merger with GUI in Python

--- pdfMerger.py
+++ pdfMerger.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfMerger
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter import *
 from tkinter import ttk
 import webbrowser
@@ -8,86 +8,120 @@ FILES = []
 
 
 def open_files():
-    files = filedialog.askopenfilenames(initialdir="",
-                                        title="Choose PDF Files",
-                                        filetypes=[("PDF Files", "*.pdf")])
-    for file in files:
-        FILES.append(file)
+    try:
+        files = filedialog.askopenfilenames(initialdir="",
+                                            title="Choose PDF Files",
+                                            filetypes=[("PDF Files", "*.pdf")])
 
-    update_treeview()
+        if files:
+            for file in files:
+                FILES.append(file)
+
+            update_treeview()
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while opening files: {str(e)}")
 
 
 def remove_file():
-    selected_items = treeview.selection()
-    for item in selected_items:
-        index = int(item.split(' ')[0]) - 1  # Extract index from the item ID
-        del FILES[index]
+    try:
+        selected_items = treeview.selection()
+        for item in selected_items:
+            index = int(item.split(' ')[0]) - 1  # Extract index from the item ID
+            del FILES[index]
 
-    update_treeview()
+        update_treeview()
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while removing file: {str(e)}")
 
 
 def move_up():
-    selected_items = treeview.selection()
-    selected_indices = [int(item.split(' ')[0]) - 1 for item in selected_items]
+    try:
+        selected_items = treeview.selection()
+        selected_indices = [int(item.split(' ')[0]) - 1 for item in selected_items]
 
-    for index in selected_indices:
-        if 0 < index < len(FILES):
-            FILES[index], FILES[index - 1] = FILES[index - 1], FILES[index]
+        for index in selected_indices:
+            if 0 < index < len(FILES):
+                FILES[index], FILES[index - 1] = FILES[index - 1], FILES[index]
 
-    update_treeview()
-    reselect_items(selected_indices, move_up=True)
+        update_treeview()
+        reselect_items(selected_indices, move_up=True)
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while moving files up: {str(e)}")
 
 
 def move_down():
-    selected_items = reversed(treeview.selection())
-    selected_indices = [int(item.split(' ')[0]) - 1 for item in selected_items]
+    try:
+        selected_items = reversed(treeview.selection())
+        selected_indices = [int(item.split(' ')[0]) - 1 for item in selected_items]
 
-    for index in selected_indices:
-        if 0 <= index < len(FILES) - 1:
-            FILES[index], FILES[index + 1] = FILES[index + 1], FILES[index]
+        for index in selected_indices:
+            if 0 <= index < len(FILES) - 1:
+                FILES[index], FILES[index + 1] = FILES[index + 1], FILES[index]
 
-    update_treeview()
-    reselect_items(selected_indices, move_up=False)
+        update_treeview()
+        reselect_items(selected_indices, move_up=False)
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while moving files down: {str(e)}")
 
 
 def clear_all():
-    global FILES
-    FILES = []
-    update_treeview()
+    try:
+        global FILES
+        FILES = []
+        update_treeview()
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while clearing files: {str(e)}")
 
 
 def merge_files():
-    if FILES != []:
-        merger = PdfMerger()
-        for pdf in FILES:
-            merger.append(pdf, import_outline=False)
-        location = filedialog.asksaveasfile(initialdir="",
-                                            title="Merge Files",
-                                            filetypes=[("PDF", "*.pdf")],
-                                            defaultextension=".pdf")
-        if location is not None:
-            merger.write(location.name)
-            merger.close()
+    try:
+        if FILES:
+            merger = PdfMerger()
+            for pdf in FILES:
+                merger.append(pdf, import_outline=False)
 
-            webbrowser.open(location.name)
+            location = filedialog.asksaveasfile(initialdir="",
+                                                title="Merge Files",
+                                                filetypes=[("PDF", "*.pdf")],
+                                                defaultextension=".pdf")
+
+            if location:
+                merger.write(location.name)
+                merger.close()
+                webbrowser.open(location.name)
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while merging files: {str(e)}")
 
 
 def update_treeview():
-    treeview.delete(*treeview.get_children())
+    try:
+        treeview.delete(*treeview.get_children())
 
-    for i, file in enumerate(FILES, start=1):
-        iid = f"{i} {file}"
-        treeview.insert("", "end", iid=iid, values=(file.split('/')[-1],))
+        for i, file in enumerate(FILES, start=1):
+            iid = f"{i} {file}"
+            treeview.insert("", "end", iid=iid, values=(file.split('/')[-1],))
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while updating the treeview: {str(e)}")
 
 
 def reselect_items(selected_indices, move_up=True):
-    for index in selected_indices:
-        new_index = index - 1 if move_up else index + 1
+    try:
+        for index in selected_indices:
+            new_index = index - 1 if move_up else index + 1
 
-        # Check if the new index is within the bounds of the updated FILES list
-        if 0 <= new_index < len(FILES):
-            new_item = f"{new_index + 1} {FILES[new_index]}"
-            treeview.selection_add(new_item)
+            if 0 <= new_index < len(FILES):
+                new_item = f"{new_index + 1} {FILES[new_index]}"
+                treeview.selection_add(new_item)
+
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while reselecting items: {str(e)}")
 
 
 if __name__ == '__main__':
@@ -95,7 +129,6 @@ if __name__ == '__main__':
     root.title("PDF Merger")
     root.resizable(width=False, height=False)
 
-    # Create entry + buttons
     clear = Button(root, text="Clear All", command=clear_all)
     open_button = Button(root, text="Open Files", command=open_files)
     remove_button = Button(root, text="Delete Selected", command=remove_file)
@@ -103,7 +136,6 @@ if __name__ == '__main__':
     move_up_button = Button(root, text="Move Up", command=move_up)
     move_down_button = Button(root, text="Move Down", command=move_down)
 
-    # Position entry + buttons
     clear.grid(row=0, column=0, padx=10, pady=10, sticky=W + E + N + S)
     open_button.grid(row=0, column=1, padx=10, pady=10, sticky=W + E + N + S)
     remove_button.grid(row=0, column=2, padx=10, pady=10, sticky=W + E + N + S)
@@ -111,12 +143,10 @@ if __name__ == '__main__':
     move_up_button.grid(row=2, column=1, padx=10, pady=10, sticky=W + E + N + S)
     move_down_button.grid(row=2, column=2, padx=10, pady=10, sticky=W + E + N + S)
 
-    # Create Treeview for displaying selected files
     treeview = ttk.Treeview(root, columns=("Files",), show="headings")
     treeview.heading("Files", text="Files")
     treeview.grid(row=1, column=0, columnspan=4, padx=10, pady=10, sticky=W + E + N + S)
 
-    # Center window
     root.eval('tk::PlaceWindow . center')
 
     root.mainloop()
